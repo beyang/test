b61389d97839e4d4e3aced5a49e4817abd722ff7
Beyang Liu <beyang@sourcegraph.com>
2018-10-25 15:40:02 -0700

    .

---
+++ hook-pre-push-wrapper.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+# From https://git-scm.com/docs/githooks#_pre_push:
+#
+# > This hook is called by git-push[1] and can be used to prevent a push from taking place. The hook
+# > is called with two parameters which provide the name and location of the destination remote, if
+# > a named remote is not being used both values will be the same.
+# >
+# > Information about what is to be pushed is provided on the hook’s standard input with lines of
+# > the form:
+# >
+# > <local ref> SP <local sha1> SP <remote ref> SP <remote sha1> LF
+# >
+# > For instance, if the command git push origin master:foreign were run the hook would receive a
+# > line like the following:
+# >
+# > refs/heads/master 67890 refs/heads/foreign 12345
+# >
+# > although the full, 40-character SHA-1s would be supplied. If the foreign ref does not yet exist
+# > the <remote SHA-1> will be 40 0. If a ref is to be deleted, the <local ref> will be supplied as
+# > (delete) and the <local SHA-1> will be 40 0. If the local commit was specified by something
+# > other than a name which could be expanded (such as HEAD~, or a SHA-1) it will be supplied as it
+# > was originally given.
+# >
+# > If this hook exits with a non-zero status, git push will abort without pushing
+# > anything. Information about why the push is rejected may be sent to the user by writing to
+# > standard error.
+
+set -euo pipefail
+
+function usage() {
+    cat <<'EOF'
+Usage: ./hook-pre-push-wrapper.sh $check_script
+
+$check_script should be the path to a script that uses the following environment variables:
+       $remote
+       $remote_url
+       $local_ref
+       $local_sha
+       $remote_ref
+       $remote_sha
+
+The following environment variables must be set (https://github.com/typicode/husky/blob/master/DOCS.md#access-git-params-and-stdin):
+       $HUSKY_GIT_STDIN should be `$remote $remote_url`
+       $HUSKY_GIT_PARAMS should contain lines of the form `$local_ref $local_sha $remote_ref $remote_sha`
+EOF
+    exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
+fi
+check_script="$1"
+
+if [ ! -v HUSKY_GIT_STDIN ] || [ ! -v HUSKY_GIT_PARAMS ]; then
+    usage
+fi
+
+
+read -ra git_params <<< "$HUSKY_GIT_PARAMS"
+if [ "${#git_params[@]}" -ne 2 ]; then usage; fi
+
+remote="${git_params[0]}"
+remote_url="${git_params[1]}"
+if [ -z "$remote" ] || [ -z "$remote_url" ]; then usage; fi
+
+while read -ra line
+do
+    if [ "${#line[@]}" -ne 4 ]; then usage; fi
+    local_ref="${line[0]}"
+    local_sha="${line[1]}"
+    remote_ref="${line[2]}"
+    remote_sha="${line[3]}"
+    if [ -z "$local_ref" ] || [ -z "$local_sha" ] || [ -z "$remote_ref" ] || [ -z "$remote_sha" ]; then usage; fi
+
+    $check_script
+done <<< "$HUSKY_GIT_STDIN"
