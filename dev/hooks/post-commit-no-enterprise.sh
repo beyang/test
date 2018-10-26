fb47ede9418a8f28a636137711edc926e51617bb
Beyang Liu <beyang@sourcegraph.com>
2018-10-25 17:03:03 -0700

    .

--- dev/hooks/post-commit-no-enterprise.sh
+++ dev/hooks/post-commit-no-enterprise.sh
@@ -4,4 +4,14 @@ set -euo pipefail
 
 cd "$(git rev-parse --show-toplevel)"
 
-remote_url=TODO local_sha="$(git rev-parse HEAD) ./dev/hooks/check-no-enterprise.sh
+
+function currentRemote() {
+    currentBranch="$(git rev-parse --abbrev-ref HEAD)"
+    if [ -z "$currentBranch" ]; then
+        return 0
+    fi
+    currentRemote="$(git config --get branch.${currentBranch}.remote)"
+    git config --get "remote.${currentRemote}.url"
+}
+
+remote_url="$(currentRemote)" local_sha="$(git rev-parse HEAD) ./dev/hooks/check-no-enterprise.sh
